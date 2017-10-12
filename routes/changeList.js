a385d4d98517ad23ce8e438ec434f4c460f3768f
pauly <pauly@clarkeology.com>
2017-10-12 21:13:25 +0000

    :construction: server, now playing from lists and searches

--- routes/changeList.js
+++ routes/changeList.js
@@ -2,7 +2,9 @@ const mpc = require('../lib/mpc')
 
 module.exports = {
   handler(req, res, next) {
-    var cmd =  'mpc clear; mpc load "' + mpc.clean(req.query.v) + '"; mpc playlist'
+    console.log('@todo instead of clearing, only list playist contents, only clear when playing!')
+    // var cmd = 'mpc clear; mpc load "' + mpc.clean(req.query.v) + '"; mpc playlist'
+    var cmd = 'mpc playlist "' + mpc.clean(req.query.v) + '"'
     mpc.handler(cmd, req, res, next)
   }
 }
