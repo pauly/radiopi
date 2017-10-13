0f3cf4b5dbca1c3ce498f45b5d7e3e5b0649352e
pauly <pauly@clarkeology.com>
2017-10-13 14:57:54 +0100

    :scissors: remove views we are all json

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 var express = require('express')
 var path = require('path')
-var favicon = require('serve-favicon')
+// var favicon = require('serve-favicon')
 var logger = require('morgan')
 // var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 
 var app = module.exports = express()
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
@@ -35,12 +31,10 @@ app.use(function(req, res, next) {
 })
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
+app.use(function(error, req, res, next) {
+  res.status(error.status || 500)
+  if (req.app.get('env') === 'development') {
+    return res.json({ error })
+  }
+  res.json({ error: error.message })
 })
